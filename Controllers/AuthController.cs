7c2f9b98c23696f620a9e51c97b1e35349c5b6e5
Iqra562 <iqra44938@gmail.com>
2023-10-21 16:11:44 +0500

    Authentication

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(UserRegisterRequest request)
+        {
+            if (ModelState.IsValid)
+
+            {
+                return Ok(request);
+
+            }
+            else
+            {
+                return View(request);
+            }
+
+        }
+    }
+}
---
+++ Models/UserRegisterRequest.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication9.Models
+{
+    public class UserRegisterRequest
+    {
+        [Required,EmailAddress]
+        [Display(Name="User Email", Prompt = "Enter Email ...")]
+        public string Email { get; set; }
+
+
+        [Required,MinLength(6,ErrorMessage ="Atleast 6 charcter required")]
+        [Display(Name = "Password", Prompt = "Password")]
+
+        public string Password { get; set; }
+
+      
+        [Required, MinLength(6, ErrorMessage = "Atleas 6 character required")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        [Display(Name = "Confirm Password", Prompt = "Confirm Password ")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
